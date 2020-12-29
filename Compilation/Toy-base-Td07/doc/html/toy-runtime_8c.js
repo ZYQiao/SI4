d82ed32bff558d40522c972800842447d3d44df4
ZYQiao <zhaoyuqiao97@gmail.com>
2020-12-30 00:56:19 +0100

    cc

---
+++ Compilation/Toy-base-Td07/doc/html/toy-runtime_8c.js
@@ -0,0 +1,6 @@
+var toy_runtime_8c =
+[
+    [ "_toy_print_bool", "toy-runtime_8c.html#afeec48f3ae57866893179067a055f3c7", null ],
+    [ "_toy_print_int", "toy-runtime_8c.html#a3caa3f94b75de63d3b5dc956cba19413", null ],
+    [ "_toy_print_string", "toy-runtime_8c.html#a84a6939ae3a550d105e10486d43f01fa", null ]
+];
---
+++ Compilation/Toy-base-Td07/doc/html/toy-runtime_8h.js
@@ -0,0 +1,7 @@
+var toy_runtime_8h =
+[
+    [ "_toy_string", "toy-runtime_8h.html#a61f22124740de12125b54bedb3351388", null ],
+    [ "_toy_print_bool", "toy-runtime_8h.html#afeec48f3ae57866893179067a055f3c7", null ],
+    [ "_toy_print_int", "toy-runtime_8h.html#a3caa3f94b75de63d3b5dc956cba19413", null ],
+    [ "_toy_print_string", "toy-runtime_8h.html#a84a6939ae3a550d105e10486d43f01fa", null ]
+];
